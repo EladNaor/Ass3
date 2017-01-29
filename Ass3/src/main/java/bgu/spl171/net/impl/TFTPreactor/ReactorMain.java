aff4435701a52b9722c5c8a75692044a5c013c57
Dor <eladna812@gmail.com>
2017-01-29 21:22:16 +0200

    dvs

---
+++ Ass3/src/main/java/bgu/spl171/net/impl/TFTPreactor/ReactorMain.java
@@ -0,0 +1,7 @@
+package bgu.spl171.net.impl.TFTPreactor;
+
+/**
+ * Created by Dor on 1/29/2017.
+ */
+public class ReactorMain {
+}
---
+++ Ass3/src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
@@ -0,0 +1,7 @@
+package bgu.spl171.net.impl.TFTPtpc;
+
+/**
+ * Created by Dor on 1/29/2017.
+ */
+public class TPCMain {
+}
