5ffca3bd46c60e43dce67f0b59856c4a5cb9da03
Dor <eladna812@gmail.com>
2017-01-29 21:26:27 +0200

    dvs

--- Ass3/src/main/java/bgu/spl171/net/impl/TFTPreactor/ReactorMain.java
+++ Ass3/src/main/java/bgu/spl171/net/impl/TFTPreactor/ReactorMain.java
@@ -1,7 +1,17 @@
 package bgu.spl171.net.impl.TFTPreactor;
 
-/**
- * Created by Dor on 1/29/2017.
- */
+import bgu.spl171.net.api.MessageEncDec;
+import bgu.spl171.net.impl.TFTP.BidiMessagingProtocolImpl;
+import bgu.spl171.net.srv.Server;
+
 public class ReactorMain {
+
+    public static void main(String[] args) {
+        Server.reactor(
+                Runtime.getRuntime().availableProcessors(),
+                Integer.valueOf(args[0]), //port
+                BidiMessagingProtocolImpl::new, //protocol factory
+                MessageEncDec::new //message encoder decoder factory
+        ).serve();
+    }
 }
--- Ass3/src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
+++ Ass3/src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
@@ -1,7 +1,16 @@
 package bgu.spl171.net.impl.TFTPtpc;
 
-/**
- * Created by Dor on 1/29/2017.
- */
+import bgu.spl171.net.api.MessageEncDec;
+import bgu.spl171.net.impl.TFTP.BidiMessagingProtocolImpl;
+import bgu.spl171.net.srv.Server;
+
 public class TPCMain {
+
+    public static void main(String[] args) {
+        Server.threadPerClient(
+                Integer.valueOf(args[0]), //port
+                BidiMessagingProtocolImpl::new, //protocol factory
+                MessageEncDec::new //message encoder decoder factory
+        ).serve();
+    }
 }
